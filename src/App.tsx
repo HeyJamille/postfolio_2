d1378f52e52b1c49fe773855d82cd98c08b95e8b
Jamille <jamille.araujo.freitas2003@gmail.com>
2024-07-26 18:11:28 -0300

    [FIX] fixed layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ import Header from './components/Header'
 import Home from './pages/Home'
 
 function App() {
-
   return (
-    <main className="p-5 md:p-10">
+    <main className="h-screen p-5 md:p-10">
       <Header />
       <Home />
       <Footer />
